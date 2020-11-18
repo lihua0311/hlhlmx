e173cb24903ff0a3bcc988adaf04c9eb25e18322
lihua0311 <74294935+lihua0311@users.noreply.github.com>
2020-11-18 17:23:16 +0800

    Create 111803.分段函数.c

---
+++ 111803.分段函数.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int x,y;
+    printf("输入 x:");
+    scanf("%d",&x);
+    if(x<1)
+    {
+        y=x;
+        printf("x=%3d,y=x=%d\n",x,y);
+    }
+    else if(x<10)
+    {
+        y=2*x-1;
+        printf("x=%d,y=2*x-1=%d\n",x,y);
+    }
+    else
+    {
+        y=3*x-11;
+    printf("x=%d,y=%d\n",x,y);
+    }
+
+}
