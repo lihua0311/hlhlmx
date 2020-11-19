3515b68d697a7e950ce689d925e155994e0910c4
lihua0311 <74294935+lihua0311@users.noreply.github.com>
2020-11-19 20:26:46 +0800

    Update and rename 2020111201.双函数.c to 111901.性别.c

---
+++ 111901.性别.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int x;
+    printf("输入:");
+    scanf("%d",&x);
+    if(x>=1&&x<=1)
+    {
+        printf("男");
+    }
+    else if(x<=0&&x>=0)
+    {
+        printf("女");
+    }
+    else{}
+}
