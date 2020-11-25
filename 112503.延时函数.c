9ef2166002a080366e304cb14747e0d620110384
lihua0311 <74294935+lihua0311@users.noreply.github.com>
2020-11-25 16:50:04 +0800

    Create 112503.延时函数.c

---
+++ 112503.延时函数.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<Windows.h>
+void main()
+{
+   printf("男：天气冷了，有个成语叫忘穿秋裤，所以今天的你穿秋裤了吗？\n");
+   Sleep(5000);
+   printf("女：宝贝，你好憨，爱你呀，宝贝，谢谢宝贝关心！\n");
+   Sleep(5000);
+   printf("男：我也爱你呀宝贝！\n");
+system("pause");
+}
