4bafb637af5819af2985fa4df46ae58dc0878e05
lihua0311 <74294935+lihua0311@users.noreply.github.com>
2020-11-26 21:01:24 +0800

    Create 112603.升序排序.app.c

---
+++ 112603.升序排序.app.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,t;
+    printf("升序排序\n请输入三个数字：\n");
+    scanf("%d，%d，%d",&a,&b,&c);
+    if(a>b)
+    {
+        t=a;
+        a=b;
+        b=t;
+
+    }
+    if(a>c)
+    {
+        t=a;
+        a=c;
+        c=t;
+    }
+    if(b>c)
+    {
+        t=b;
+        b=c;
+        c=t;
+    }
+    printf("%d\n%d\n%d\n",a,b,c);
+}
