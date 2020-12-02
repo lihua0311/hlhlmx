a7b88c23700f432da031e528c79754cb9bc386ed
lihua0311 <74294935+lihua0311@users.noreply.github.com>
2020-12-02 17:18:17 +0800

    Update 120204.数据类型的转换.app.c

--- 120204.数据类型的转换.app.c
+++ 120204.数据类型的转换.app.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 void main()
 {
-    double a=2.032529607;
-    printf("%d",(int)a);
+    double a;
+    printf("                                            欢迎使用此程序\n");
+    printf("请输入一个小数：");
+    scanf("%lf",&a);
+    printf("整数为：%d",(int)a);
+    system("pause");
 }
