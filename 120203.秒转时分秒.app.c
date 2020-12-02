5e7a27fa39c3dcc7350f89caf614794e7f134806
lihua0311 <74294935+lihua0311@users.noreply.github.com>
2020-12-02 17:00:39 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -2,6 +2,7 @@
 void main()
 {
     int a,b,c,x;
+    printf("                                                欢迎使用此程序\n");
     printf("请输入一个秒数：");
     scanf("%d",&x);
     if(x>=0&&x<=86399)
@@ -9,7 +10,7 @@ void main()
         a=x/3600;
         b=x%3600/60;
         c=x%3600%60;
-        printf("%d时%d分%d秒\n",a,b,c);
+        printf("%d时%d分%d秒\n谢谢使用！\n",a,b,c);
     }
     system("pause");
 }
