73279f1d68fd2e3b8beeb372923971062803ebab
lihua0311 <74294935+lihua0311@users.noreply.github.com>
2020-12-09 17:17:44 +0800

    Create 120905.串长度.app.c

---
+++ 120905.串长度.app.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    char a1[]="shabichensong";
+    char a2[]="傻逼卢拯";
+    char a3[]="傻逼贺冠华";
+    printf("%d\n",sizeof(a1));
+    printf("%d\n",sizeof(a2));
+    printf("%d\n",sizeof(a3));
+}
+
