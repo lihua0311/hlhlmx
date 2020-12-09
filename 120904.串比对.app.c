cce2c214cab48007e8199aa0ba8a69c6d12f85f9
lihua0311 <74294935+lihua0311@users.noreply.github.com>
2020-12-09 16:48:26 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char a1[]="傻逼陈松";
+    char a2[]="傻逼卢拯";
+    if(strcmp(a1,a2)==0)
+    {
+        printf("两个傻逼\n");
+    }
+    else
+    {
+        printf("两个大傻逼！\n");
+    }
+}
+
